dd5f36b07ec0e3e5b394695d22b8dfc1af65935a
rishirishhh <rishikavya19@gmail.com>
2025-06-24 01:38:38 +0530

    feat: added hls encoder

---
+++ src/pkg/ffmpeg/image.go
@@ -0,0 +1,20 @@
+package ffmpeg
+
+import (
+	"os/exec"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func ConvertImg(srcpath string, dstpath string) error {
+	_, err := exec.Command("ffmpeg", "-i", srcpath, "-vf",
+		"crop='if(gt(iw, ih), ih, iw)':'if(gt(iw, ih), ih, iw )':'if(gt(iw, ih), (iw-ih)/2, 0)':'if(gt(iw,ih), 0, (ih-iw)/2)', scale=250:250",
+		dstpath).CombinedOutput()
+
+	if err != nil {
+		return err
+	}
+	log.Debug("Image converted")
+
+	return nil
+}
---
+++ src/pkg/ffmpeg/transformations.go
@@ -0,0 +1,49 @@
+package ffmpeg
+
+import (
+	"context"
+	"io"
+	"os/exec"
+)
+
+func CreateFlipCommand(ctx context.Context) *exec.Cmd {
+	// Create Command
+	command := "ffmpeg"
+	args := []string{"-i", "pipe:0"}
+	args = append(args, "-f", "mpegts", "-muxdelay", "0", "-map", "0:0", "-map", "0:1", "-acodec", "copy")
+	args = append(args, "-vcodec", "libx264", "-preset", "fastlibx264", "-preset", "superfast", "-copyts")
+	args = append(args, "-vf", "vflip")
+	args = append(args, "pipe:1")
+
+	return exec.CommandContext(ctx, command, args...)
+}
+
+func CreateGrayCommand(ctx context.Context) *exec.Cmd {
+	// Create command
+	command := "ffmpeg"
+	args := []string{"-i", "pipe:0"}
+	args = append(args, "-f", "mpegts", "-muxdelay", "0", "-map", "0:0", "-map", "0:1", "-acodec", "copy")
+	args = append(args, "-vcodec", "libx264", "-preset", "fastlibx264", "-preset", "superfast", "-copyts")
+	args = append(args, "-vf", "hue=s=0")
+	args = append(args, "pipe:1")
+	return exec.CommandContext(ctx, command, args...)
+}
+
+func TransformHLSPart(cmd *exec.Cmd, stdin io.Reader, stdout io.Writer) error {
+	cmd.Stdin = stdin
+	cmd.Stdout = stdout
+
+	// Execute Command
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	// Wait end of ffmpeg command
+	err = cmd.Wait()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
