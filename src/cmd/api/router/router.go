4ad6c7613261e45bcf215b1caba63c0c26ef4a51
rishirishhh <rishikavya19@gmail.com>
2025-09-02 00:17:10 +0530

    feat: added videos uploading route

--- src/cmd/api/router/router.go
+++ src/cmd/api/router/router.go
@@ -1,12 +1,14 @@
 package router
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/rishirishhh/vought/src/cmd/api/config"
 	"github.com/rishirishhh/vought/src/cmd/api/controllers"
+	"github.com/rishirishhh/vought/src/cmd/api/db/dao"
 	"github.com/rishirishhh/vought/src/pkg/clients"
 )
 
@@ -17,14 +19,23 @@ type Clients struct {
 	UUIDGen               clients.IUUIDGenerator
 }
 
+type DAOs struct {
+	Db         *sql.DB
+	VideosDAO  dao.VideosDAO
+	UploadsDAO dao.UploadsDAO
+}
+
 type responseWriter struct {
 	http.ResponseWriter
 	statusCode int
 }
 
-func NewRouter(config config.Config, clients *Clients) http.Handler {
+func NewRouter(config config.Config, clients *Clients, DAOs *DAOs) http.Handler {
 	r := mux.NewRouter()
 	r.PathPrefix("/ws").Handler(controllers.WSHandler{Config: config, AmqpVideoStatusUpdate: clients.AmqpVideoStatusUpdate}).Methods("GET")
+
+	v1 := r.PathPrefix("/api/v1").Subrouter()
+	v1.PathPrefix("/videos/upload").Handler(controllers.VideoUploadHandler{S3Client: clients.S3Client, AmqpClient: clients.AmqpClient, AmqpVideoStatusUpdate: clients.AmqpVideoStatusUpdate, VideosDAO: &DAOs.VideosDAO, UploadsDAO: &DAOs.UploadsDAO, UUIDGen: clients.UUIDGen}).Methods("POST")
 	return handlers.CORS(getCORS())(r)
 }
 
