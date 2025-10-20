1d950a7586746a0cb907ccfc3ecbca8d54e9decb
rishirishhh <rishikavya19@gmail.com>
2025-10-21 01:52:35 +0530

    feat: added video cover fetching ability

---
+++ src/cmd/api/controllers/video_cover_handler.go
@@ -0,0 +1,73 @@
+package controllers
+
+import (
+	b64 "encoding/base64"
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rishirishhh/vought/src/cmd/api/db/dao"
+	"github.com/rishirishhh/vought/src/pkg/clients"
+	log "github.com/sirupsen/logrus"
+)
+
+type VideoCoverHandler struct {
+	S3Client  clients.IS3Client
+	VideosDAO *dao.VideosDAO
+	UUIDGen   clients.IUUIDGenerator
+}
+
+// VideoCoverHandler godoc
+// @Summary Get video cover image in base64
+// @Description Get video cover image in base64
+// @Tags video
+// @Accept plain
+// @Produce plain
+// @Param id path string true "Video ID"
+// @Success 200 {string} string "video cover image in base64"
+// @Failure 400 {string} string
+// @Failure 404 {string} string
+// @Failure 500 {string} string
+// @Router /api/v1/videos/{id}/cover [get]
+func (v VideoCoverHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	log.Debug("GET VideoCoverHandler - parameters ", vars)
+
+	id := vars["id"]
+	if !v.UUIDGen.IsValidUUID(id) {
+		log.Error("Invalid id")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Fetch video cover path from DB
+	video, err := v.VideosDAO.GetVideo(r.Context(), id)
+	if err != nil {
+		log.Error("Failed to get video "+id+" info from DB: ", err)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	// If there is no cover file, return nothing, but no error
+	if video.CoverPath != "" {
+		// Fetch cover image from S3
+		object, err := v.S3Client.GetObject(r.Context(), video.CoverPath)
+		if err != nil {
+			log.Error("Failed to open video cover "+video.CoverPath+": ", err)
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		rawObject, err := io.ReadAll(object)
+		if err != nil {
+			log.Error("Failed to convert to base64 video cover "+video.CoverPath+": ", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		b64Object := b64.StdEncoding.EncodeToString(rawObject)
+
+		w.Header().Set("Content-Type", "text/plain")
+		_, _ = w.Write([]byte(b64Object))
+	}
+}
