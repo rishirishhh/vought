1e39fc3fe332bc1d4d3a228a1be160c2d1f457b5
rishirishhh <rishikavya19@gmail.com>
2025-10-23 00:48:08 +0530

    feat: get a list of video transformers

---
+++ src/cmd/api/controllers/video_transformer_list_handler.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	jsonDTO "github.com/rishirishhh/vought/src/cmd/api/dto/json"
+
+	"github.com/rishirishhh/vought/src/pkg/clients"
+	log "github.com/sirupsen/logrus"
+)
+
+type VideoTransformerListHandler struct {
+	ServiceDiscovery clients.ServiceDiscovery
+}
+
+type TransformerServiceListResponse struct {
+	Services []jsonDTO.TransformerServiceJson `json:"services"`
+}
+
+// VideoTransformerListHandler godoc
+// @Summary Get list of existing services
+// @Description Get list of existing services
+// @Tags services
+// @Produce json
+// @Success 200 {object} TransformerServiceListResponse "Service list"
+// @Router /api/v1/videos/transformer/list [get]
+func (v VideoTransformerListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Debug("GET VideoTransformerListHandler")
+	existingServices := v.ServiceDiscovery.GetExistingServices()
+	services := []jsonDTO.TransformerServiceJson{}
+	for _, service := range existingServices {
+		services = append(services, jsonDTO.TransformerServiceToTransformerServiceJson(service))
+	}
+	payload, err := json.Marshal(TransformerServiceListResponse{Services: services})
+	if err != nil {
+		log.Error("Unable to parse data struct in json ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(payload)
+}
