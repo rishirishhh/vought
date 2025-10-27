8a607f1664283bccfe3c6fa308a445821d6f82a2
rishirishhh <rishikavya19@gmail.com>
2025-10-28 00:21:46 +0530

    feat: encoding process created

---
+++ src/cmd/encoder/encoding/encoding.go
@@ -0,0 +1,177 @@
+package encoding
+
+import (
+	"context"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/rishirishhh/vought/src/pkg/clients"
+	contracts "github.com/rishirishhh/vought/src/pkg/contracts/v1"
+	"github.com/rishirishhh/vought/src/pkg/ffmpeg"
+)
+
+func Process(s3Client clients.IS3Client, videoData *contracts.Video) error {
+	// Going to the working directory
+	processingFolder := filepath.Join(os.TempDir(), "/encoder-processing-dir")
+	if err := os.MkdirAll(processingFolder, os.ModePerm); err != nil {
+		return err
+	}
+
+	if err := os.Chdir(processingFolder); err != nil {
+		return err
+	}
+
+	defer func() {
+		// Cleaning up
+		_ = os.Chdir(os.TempDir())
+		_ = os.RemoveAll(processingFolder)
+	}()
+
+	// Download and write the source file on the filesystem
+	err := fetchVideoSource(s3Client, videoData)
+	if err != nil {
+		log.Error("Failed to fetch video source")
+		return err
+	}
+
+	// Video Processing
+	// Some video doesn't contains audio and HLS can't handle it, so we add an empty track
+	err = encode(videoData)
+	if err != nil {
+		log.Error("Failed to encode video")
+		return err
+	}
+
+	// Download and write the cover file on the filesystem
+	isCoverFetch, err := fetchCoverSource(s3Client, videoData)
+	if err != nil {
+		log.Error("Failed to fetch cover image")
+		return err
+	}
+
+	// Cover image compression
+	if isCoverFetch {
+		if err = compressCover(videoData); err != nil {
+			log.Error("Failed to compress cover image")
+			return err
+		}
+	}
+
+	log.Info("Processing of video ", videoData.GetId(), "done - Uploading to S3")
+	// Uploading files to the S3
+	err = uploadFiles(s3Client, videoData)
+	if err != nil {
+		log.Error("Failed to upload video data to S3")
+		return err
+	}
+
+	return nil
+}
+
+func fetchVideoSource(s3Client clients.IS3Client, videoData *contracts.Video) error {
+	source, err := s3Client.GetObject(context.Background(), videoData.GetSource())
+	if err != nil {
+		return err
+	}
+	f, err := os.Create(filepath.Base(videoData.GetSource()))
+	if err != nil {
+		return err
+	}
+
+	if _, err := io.Copy(f, source); err != nil {
+		return err
+	}
+
+	return f.Close()
+}
+
+func encode(data *contracts.Video) error {
+	sourceFile := filepath.Base(data.GetSource())
+
+	withSound, err := ffmpeg.CheckContainsSound(sourceFile)
+	if err != nil {
+		return err
+	}
+	if !withSound {
+		if err := ffmpeg.AddEmptyAudioTrack(sourceFile); err != nil {
+			return err
+		}
+	}
+
+	res, err := ffmpeg.ExtractResolution(sourceFile)
+	if err != nil {
+		return err
+	}
+
+	if err = ffmpeg.ConvertToHLS(sourceFile, res); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func fetchCoverSource(s3Client clients.IS3Client, videoData *contracts.Video) (isFileFetch bool, err error) {
+	// Do not fetch cover if cover path is empty
+	if len(videoData.GetCoverPath()) == 0 {
+		return false, nil
+	}
+
+	source, err := s3Client.GetObject(context.Background(), videoData.GetCoverPath())
+	if err != nil {
+		return false, err
+	}
+	f, err := os.Create(filepath.Base(videoData.GetCoverPath()))
+	if err != nil {
+		return false, err
+	}
+
+	if _, err := io.Copy(f, source); err != nil {
+		return true, err
+	}
+
+	return true, f.Close()
+}
+
+func compressCover(videoData *contracts.Video) error {
+	err := ffmpeg.ConvertImg(filepath.Base(videoData.GetCoverPath()), "cover.jpeg")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func uploadFiles(s3Client clients.IS3Client, data *contracts.Video) error {
+	err := filepath.Walk(".",
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if path == "." || (!strings.HasSuffix(path, ".ts") && !strings.HasSuffix(path, ".m3u8") && !strings.HasSuffix(path, ".jpeg")) {
+				log.Debug("Skipping ", path)
+				return nil
+			}
+			f, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer func() { _ = f.Close() }()
+			return s3Client.PutObjectInput(context.Background(), f, filepath.Join(data.GetId(), path))
+		})
+	if err != nil {
+		return err
+	}
+
+	// Remove cover image on S3 if needed
+	if _, err = os.Stat("cover.jpeg"); err == nil {
+		err = s3Client.RemoveObject(context.Background(), data.GetCoverPath())
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
