08475917e62f00cc485d76e28c3db8536d2b8b9e
rishirishhh <rishikavya19@gmail.com>
2025-10-28 17:48:36 +0530

    feat: encoder completed

--- src/cmd/encoder/main.go
+++ src/cmd/encoder/main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"github.com/rishirishhh/vought/src/cmd/api/config"
+	"github.com/rishirishhh/vought/src/cmd/encoder/eventhandler"
+	"github.com/rishirishhh/vought/src/pkg/clients"
+	log "github.com/sirupsen/logrus"
+)
+
 func main() {
+	log.Info("Starting Vought Encoder")
+
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatal("Cannot parse Env var ", err)
+	}
+
+	if cfg.DevMode {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	// S3 client to access the videos
+	s3Client, err := clients.NewS3Client(cfg.S3Host, cfg.S3Region, cfg.S3Bucket, cfg.S3AuthKey, cfg.S3AuthPwd)
+	if err != nil {
+		log.Fatal("Fail to create S3Client ", err)
+	}
+	amqpClientVideoUpload, _ := clients.NewAmqpClient(cfg.RabbitmqUser, cfg.RabbitmqPwd, cfg.RabbitmqAddr)
+
+	// Listen, consume and publish on amqpClientVideoUpload
+	eventhandler.ConsumeEvents(amqpClientVideoUpload, s3Client)
+
 }
